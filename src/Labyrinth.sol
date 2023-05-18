28285a8bb9ea848e149a2bad273cb8e0871bbe6d
kadenzipfel <kaden.zipfel@hotmail.com>
2023-05-18 13:39:19 -0700

    Add empty functions

--- src/Labyrinth.sol
+++ src/Labyrinth.sol
@@ -4,5 +4,18 @@ pragma solidity 0.8.19;
 import {IPuzzle} from "./IPuzzle.sol";
 
 contract Labyrinth is IPuzzle {
+    /// @inheritdoc IPuzzle
+    function name() external pure returns (string memory) {
+        return "Labyrinth";
+    }
 
+    /// @inheritdoc IPuzzle
+    function generate(address _seed) external returns (uint256) {
+        
+    }
+
+    /// @inheritdoc IPuzzle
+    function verify(uint256 _start, uint256 _solution) external returns (bool) {
+        
+    }
 }
