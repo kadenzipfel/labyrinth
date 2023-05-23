452fa1ed11ce8244419563523bb75ffa343f149a
kadenzipfel <kaden.zipfel@hotmail.com>
2023-05-22 18:33:17 -0700

    Use proper solution

--- test/Labyrinth.t.sol
+++ test/Labyrinth.t.sol
@@ -16,14 +16,14 @@ contract LabyrinthTest is Test {
         assertTrue(labyrinth.verify(0, solution));
     }
 
-    // function testTraverseWalls() public {
-    //     vm.roll(470);
-    //     uint256 seed = labyrinth.generate(address(0x3111327EdD38890C3fe564afd96b4C73e8101747));
+    function testTraverseWalls() public {
+        vm.roll(524);
+        uint256 seed = labyrinth.generate(address(0x3111327EdD38890C3fe564afd96b4C73e8101747));
 
-    //     assertEq(seed, 0x2647b8817a2f4008e7af6648c7bf16a9253a42329c551415433090a569075505);
+        assertEq(seed, 0x04020d050d0e54f1031285c9e3fb5283a1c81b8d90016e3a6521244c38eaa307);
 
-    //     assertTrue(labyrinth.verify(seed, 0x7d75577777fddfd));
-    // }
+        assertTrue(labyrinth.verify(seed, 0x5ff5f7fd5557ddf));
+    }
 
     /// forge-config: default.fuzz.runs = 100000
     function testCannotTraverseWallsRandomSolution(uint256 solution) public {
