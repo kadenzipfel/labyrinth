1965c2c9d37838be4066fc38e9401cfb46a81723
kadenzipfel <kaden.zipfel@hotmail.com>
2023-05-24 18:36:56 -0700

    testTraverseWalls

--- test/LabyrinthHuff.t.sol
+++ test/LabyrinthHuff.t.sol
@@ -29,4 +29,13 @@ contract LabyrinthHuffTest is Test {
         uint256 solution = 0x777777777777777;
         assertTrue(labyrinth.verify(0, solution));
     }
+
+    function testTraverseWalls() public {
+        vm.roll(524);
+        uint256 seed = labyrinth.generate(address(0x3111327EdD38890C3fe564afd96b4C73e8101747));
+
+        assertEq(seed, 0x04020d050d0e54f1031285c9e3fb5283a1c81b8d90016e3a6521244c38eaa307);
+
+        assertTrue(labyrinth.verify(seed, 0x5ff5f7fd5557ddf));
+    }
 }
